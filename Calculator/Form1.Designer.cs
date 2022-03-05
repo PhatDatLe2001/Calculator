e844c5686d61b82482227f77ad739f6f19d2a1e8
SabertoothSBT <phatdatvn2001@gmail.com>
2022-03-05 12:01:29 +0700

    Cập nhật giao diện

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -29,13 +29,104 @@ namespace Calculator
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtKetQua = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Số a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 28);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Số b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 28);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Kết quả";
+            // 
+            // txtKetQua
+            // 
+            this.txtKetQua.Location = new System.Drawing.Point(146, 188);
+            this.txtKetQua.Name = "txtKetQua";
+            this.txtKetQua.Size = new System.Drawing.Size(203, 34);
+            this.txtKetQua.TabIndex = 1;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(146, 131);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(203, 34);
+            this.txtB.TabIndex = 1;
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(146, 78);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(203, 34);
+            this.txtA.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightGray;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(262, 243);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Cộng";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(391, 306);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtKetQua);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtKetQua;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Button button1;
     }
 }
 
